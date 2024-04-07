769783d609042152ddb48feb6ff0b5120bfc6d8c
JonathanG321 <JonathanG321@ymail.com>
2024-04-07 17:58:29 -0400

    test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,7 +8,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix The test worked</h1>
       <ul>
         <li>
           <Link to={'/test'}>Test</Link>
